f2b58a0d20c0b4c09dc59311b5980520910a4767
DavidBatkay <david.batkay@emanuel.ro>
2023-10-16 22:25:19 +0300

    s

---
+++ impartire2nr/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace impartire2nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("a= ");
+            a=int.Parse(Console.ReadLine());
+            Console.WriteLine("b= ");
+            b=int.Parse(Console.ReadLine());
+            if (a % b != 0)
+                Console.WriteLine("");
+            else
+                Console.WriteLine("");
+            
+        }
+    }
+}
